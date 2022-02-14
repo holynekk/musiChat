5e05fe394161dc9f0154ea43b96bd8f7c255a0b7
holynekk <e238109@metu.edu.tr>
2022-02-15 01:10:17 +0300

    styling & new components

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Redirect} from "react-router-dom";
+import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<p>This is the homepage</p>} />
+                    <Route path='/join' element={<RoomJoinPage />} />
+                    <Route path='/create' element={<CreateRoomPage />} />
+                </Routes>
+            </Router>
+        );
+    }
+}
